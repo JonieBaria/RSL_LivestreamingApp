77e9f5e49d66deb8728e7bba1274c3d22087effc
Jonie <joniebariaDE@gmail.com>
2025-03-16 21:37:48 +0800

    http

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ const IPCameraViewer = () => {
 
   const handleConnect = () => {
     if (ip) {
-      setStreamUrl(`https://${ip}:8080/video`);
+      setStreamUrl(`http://${ip}:8080/video`);
     }
   };
 
